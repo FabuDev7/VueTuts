8ad09fab9d7f0fa21e4861d0de5d53768b8f7984
FabioTarantinoMD <fabio.tarantino@manydesigns.com>
2022-04-21 10:38:53 +0200

    Prova commit

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <ButtonComp text="Add Task" color="green" />
+  </header>
+</template>
+
+<script>
+import ButtonComp from './Button'
+
+export default {
+  name: 'HeaderComp',
+  props: {
+    title: String
+  },
+  components: {
+    ButtonComp
+  }
+}
+</script>
+
+<style scoped>
+tt-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
